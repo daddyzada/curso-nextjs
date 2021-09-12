6b8b5c080d2ab60a202028bca270bfac20fa4f85
daddyzada <daddyzada77@yahoo.com>
2021-09-11 22:25:28 -0300

    Upload archives

---
+++ Projetos/Tabuleiro/components/miniComponents/Subdivisao.jsx
@@ -0,0 +1,14 @@
+import styles from '../../styles/Subdivisao.module.css'
+
+const Subdivisao = (props) => {
+    return (
+    <div
+    style={{
+        backgroundColor: props.preta ? "#000" : "#fff"
+    }} 
+    className={styles.subdivisao}
+    ></div>
+    )
+}
+
+export default Subdivisao;
